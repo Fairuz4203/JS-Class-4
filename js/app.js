572410f00049f9bd99a4d218146574f7e62ffabc
Fairuz4203 <fairuzisnat@gmail.com>
2025-03-04 17:55:16 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,25 @@
+let menuBar = document.querySelector('.menu_bar');
+
+let menuSection = document.querySelector('.menu');
+
+let cross = document.querySelector('.cross');
+
+let menuParent = document.querySelectorAll('.menu_parent li a'); // Fix: Use querySelectorAll
+
+menuBar.addEventListener('click', function(){
+   menuSection.classList.add('active_menu')
+   cross.classList.add('active_cross')
+})
+
+cross.addEventListener('click', function(){
+    menuSection.classList.remove('active_menu')
+})
+
+
+
+
+menuParent.forEach((item) => {
+    item.addEventListener('click', function () {
+        menuSection.classList.remove('active_menu');
+    });
+});
